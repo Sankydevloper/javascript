b7554b453ee57f3085b6a687f3b7a2e0cf24d0af
SANKET MOHANTY <146160933+Sankydevloper@users.noreply.github.com>
2023-11-14 16:42:03 +0000

    datatypes of JS

---
+++ 01_BASICJAVASCRIPT/01_datatype.js
@@ -0,0 +1,10 @@
+let age = 56;
+let name ="Ranjith"
+let child=false;
+let loop=89.0;
+let create=null;
+console.log(typeof age)
+console.log(typeof name)
+console.log(typeof child)
+console.log(typeof loop)
+console.log(typeof create)
